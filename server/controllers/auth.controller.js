438cfc720c78e69da57011a24110f036e2ef9e22
Nagpal45 <nagpal4503@gmail.com>
2024-07-13 16:16:09 +0530

    server setup with prisma, register route

---
+++ server/controllers/auth.controller.js
@@ -0,0 +1,56 @@
+import bcrypt from 'bcrypt';
+import prisma from '../lib/prisma.js';
+
+export const register = async (req, res) => {
+    const {username, email,password} = req.body;
+
+   try{
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await prisma.user.create({
+        data: {
+            username,
+            email,
+            password: hashedPassword
+        }
+    });
+
+    res.status(201).json({message: "User created successfully"});
+   }
+    catch(error){
+         console.log(error);
+         res.status(500).json({message: "Internal server error"});
+    }
+}
+
+export const login = async (req, res) => {
+    const {username, password} = req.body;
+
+    try{
+        const user = await prisma.user.findUnique({
+            where: {
+                username
+            }
+        });
+
+        if(!user){
+            return res.status(400).json({message: "Invalid credentials"});
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if(!isPasswordValid){
+            return res.status(400).json({message: "Invalid credentials"});
+        }
+
+        
+
+    }catch(error){
+        console.log(error);
+        res.status(500).json({message: "Internal server error"});
+    }
+}
+
+export const logout = (req, res) => {
+    console.log("logout controller");
+}
