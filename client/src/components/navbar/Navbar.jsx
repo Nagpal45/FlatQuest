28dd1cf0d641e12b998807956556ecd16a1ebac5
Nagpal45 <nagpal4503@gmail.com>
2024-07-16 02:24:09 +0530

    sockeet & zustand chat

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -2,13 +2,20 @@ import { useContext, useState } from 'react';
 import './navbar.scss';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../context/AuthContext';
+import { useNotificationStore } from '../../lib/notificationStore';
 
 function Navbar() {
   const [open, setOpen] = useState(false);
 
   const {currentUser} = useContext(AuthContext);
 
-  
+  const fetch = useNotificationStore((state) => state.fetch);
+
+  const number = useNotificationStore((state) => state.number);
+
+  if(currentUser) {
+    fetch();
+  }
 
   return (
     <nav>
@@ -27,9 +34,9 @@ function Navbar() {
           <img src={currentUser.avatar || "/noavatar.jpg"}/>
           <span>{currentUser.username}</span>
           <Link to="/profile" className='profile'>
-            <div className="notification">
-              3
-            </div>
+            {number > 0 && <div className="notification">
+              {number}
+            </div>}
             <span>Profile</span>
           </Link>
         </div>) : (
